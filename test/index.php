57ac2b8f01326a34b03fa66a9c131ae7225e4d27
Doan Duc Toan (IT N2E-HN) <doanductoan@net2e.vn>
2017-07-19 11:04:21 +0700

    test

--- test/index.php
+++ test/index.php
@@ -6,6 +6,6 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 	</head>
 	<body>
-		<script type="text/javascript"></script>
+		<h1>Test</h1>
 	</body>
 </html>
